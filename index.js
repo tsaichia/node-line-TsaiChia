1c3cd86d7f5d184143768cf4b6914a2f9b0989ac
tsaichia <61406370+tsaichia@users.noreply.github.com>
2020-05-21 16:34:58 +0800

    更新 index

--- index.js
+++ index.js
@@ -15,26 +15,113 @@ const bot = linebot({
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 })
 
+//  加入好友
+// bot.on('follow', async (event) => {
+//   let msg = ''
+//   try {
+//     msg = '哈囉囉囉~感謝你加我好友\nLOVE YOU~'
+//   } catch (error) {
+//     msg = 'Oops!發生錯誤'
+//   }
+//   event.reply(msg)
+// })
+
 // 當收到訊息時
+const getKind = async (kind) => {
+  const arr = []
+  const arr2 = []
+  let msg = []
+  const a = [{ aa: 'aa', bb: 'bb' }, { cc: 'cc', dd: 'dd' }]
+
+  a.filter(function (x) {
+    return x.aa === 'aa'
+  })
+  // console.log(b)
+  try {
+    const data = await rp({ uri: 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL', json: true })
+
+    const all = data.filter(function (x) {
+      // console.log(x.animal_kind)
+      return x.animal_kind === kind
+    })
+    // console.log(all)
+
+    for (const d of data) {
+      const ak = d.animal_kind
+      if (ak === kind) {
+        arr.push(d.animal_place)
+        arr2.push(d.album_file)
+      }
+    }
+    // console.log(arr2[0])
+    const rand = Math.floor(Math.random() * arr.length)
+    const rand2 = Math.floor(Math.random() * arr2.length)
+    msg.push({
+      type: 'image',
+      originalContentUrl: all[rand2].album_file,
+      previewImageUrl: all[rand2].album_file
+    },
+    {
+      type: 'text',
+      text: all[rand].animal_place
+    },
+    {
+      type: 'text',
+      text: all[rand].shelter_tel
+    },
+    {
+      type: 'text',
+      text: all[rand].shelter_address
+    }
+
+    )
+    console.log(all[rand].shelter_tel)
+    console.log(all[rand].animal_place)
+    console.log(all[rand].album_file)
+  } catch (error) {
+    msg = 'Oops!\u{100085}\u{100085}\n要輸入\n狗\u{10005E}或貓\u{10005F}喔!'
+  }
+  // console.log(msg)
+  return msg
+}
+getKind('貓')
+
 bot.on('message', async (event) => {
   let msg = ''
-  try {
-    const data = await rp({ uri: 'https://kktix.com/events.json', json: true })
-    msg = data.entry[0].title
-  } catch (error) {
-    msg = '發生錯誤'
+  if (event.message.type === 'text') {
+    msg = await getKind(event.message.text)
   }
+  console.log(msg)
   event.reply(msg)
 })
 
-//  學你說話
-// bot.on('message', event => {
+// bot.on('message', async (event) => {
+//   search = event.message.text
+//   let msg = ''
 //   if (event.message.type === 'text') {
-//     event.reply(event.message.text)
+//     if (search.includes('品種')) {
+//       msg = await getKind(search.substr(0, 1))
+//     } else {
+//       await getImg()
+//       msg = ad
+//     }
 //   }
+//   console.log(msg)
+//   event.reply(msg)
 // })
 
-// 在PORT移動
+// bot.on('message', async (event) => {
+//   let msg = ''
+//   try {
+//     const data = await rp({ uri: 'https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL', json: true })
+//     msg = data[0].shelter_name
+//   } catch (error) {
+//     msg = 'Oops!發生錯誤'
+//   }
+//   event.reply(msg)
+// })
+
+// 在PORT 啟動
 bot.listen('/', process.env.PORT, () => {
   console.log('機器人已啟動')
 })
